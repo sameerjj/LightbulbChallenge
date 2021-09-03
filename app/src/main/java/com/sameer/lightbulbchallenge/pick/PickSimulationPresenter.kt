8d81e112883839ee4edca98a5618e63fcaa989c6
Sameer Jiwani <jiwani.sameer@gmail.com>
2021-09-03 16:09:20 -0400

    implementation of presenter logic

--- app/src/main/java/com/sameer/lightbulbchallenge/pick/PickSimulationPresenter.kt
+++ app/src/main/java/com/sameer/lightbulbchallenge/pick/PickSimulationPresenter.kt
@@ -1,9 +1,32 @@
 package com.sameer.lightbulbchallenge.pick
 
-class PickSimulationPresenter {
+import kotlin.random.Random
+
+class PickSimulationPresenter(val view: PickSimulationView) {
 
     /// run Single simulation, determine and display number of unique colors
-    fun runSingleSimulation(numLightBulbs: Int, numColors: Int, numPicks: Int) {
+    fun runSingleSimulation(numColors: Int, numOfEachColor: Int, numPicks: Int) {
+        //map to keep track of numColor bulbs
+        val pickMap: MutableMap<Int, Int> = mutableMapOf()
+//        for (i in 1..numColors){
+//            pickMap[i] = 0
+//        }
 
+        val numBulbs = numColors*numOfEachColor
+        for (i in 1..numPicks){
+            val pick = Random.nextInt(1, numBulbs)
+            val color = pick/numOfEachColor
+            pickMap[color] = (pickMap[color] ?: 0) + 1
+        }
+
+        var uniqueColors = 0
+        for (i in 1..numColors){
+            val colorCount = pickMap[i]
+            if (colorCount != null && colorCount > 0){
+                uniqueColors += 1
+            }
+        }
+
+        view.displaySingleSimOutput(uniqueColors.toDouble())
     }
 }
