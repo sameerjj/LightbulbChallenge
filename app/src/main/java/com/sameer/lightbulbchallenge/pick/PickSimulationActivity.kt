a72bff4cb2132e747bf938a99c36e89f562b9e66
Sameer Jiwani <jiwani.sameer@gmail.com>
2021-09-03 16:09:31 -0400

    use activity to run hardcoded simulation to test

--- app/src/main/java/com/sameer/lightbulbchallenge/pick/PickSimulationActivity.kt
+++ app/src/main/java/com/sameer/lightbulbchallenge/pick/PickSimulationActivity.kt
@@ -1,12 +1,22 @@
 package com.sameer.lightbulbchallenge.pick
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.sameer.lightbulbchallenge.R
+import com.sameer.lightbulbchallenge.databinding.ActivityPickSimulationBinding
 
-class PickSimulationActivity : AppCompatActivity() {
+class PickSimulationActivity : AppCompatActivity(), PickSimulationView {
+    var presenter: PickSimulationPresenter? = null
+    lateinit var binding: ActivityPickSimulationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pick_simulation)
+        binding = ActivityPickSimulationBinding.inflate(layoutInflater)
+        presenter = PickSimulationPresenter(this)
+        presenter?.runSingleSimulation(4, 20, 50)
+    }
+
+    override fun displaySingleSimOutput(numUniqueColours: Double) {
+        binding.textview.text = numUniqueColours.toString()
     }
 }
